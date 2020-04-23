0baf5b8de51c1964e46a71513478b6c19420fba2
s-k-hassan <44696691+s-k-hassan@users.noreply.github.com>
2020-04-23 19:10:44 +1000

    Initial commit
    Install VS Code, Powershell, Chrome, Lastpass for Chrome, Update reps and reboot. Removes Firefox

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Install Packages
+sudo apt-get install git curl apt-transport-https open-vm-tools -y
+
+# Install VS Code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install code
+rm packages.microsoft.gpg
+
+# Install Powershell for Linux
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo add-apt-repository universe
+sudo apt-get install -y powershell
+rm packages-microsoft-prod.deb
+
+# Remove Firefox
+sudo apt-get remove firefox -y
+
+# Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+# Install Lastpass
+google-chrome https://chrome.google.com/webstore/detail/lastpass-free-password-ma/hdokiejnpimakedhajhdlcegeplioahd
+
+# Update everything
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
+
+# Reboot
+sudo shutdown -r now
