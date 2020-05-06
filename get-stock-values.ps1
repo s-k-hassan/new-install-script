3bcbbd80d570f18c0e15029a13d068fb3353580f
s-k-hassan <44696691+s-k-hassan@users.noreply.github.com>
2020-05-07 08:41:46 +1000

    Update get-stock-values.ps1

--- get-stock-values.ps1
+++ get-stock-values.ps1
@@ -1,10 +1,10 @@
 #! /usr/bin/pwsh
 $tick_list=@("WOW.AX","DDR.AX","QAN.AX")
-$API_key=get-content -raw ./key.txt
+$API_key=get-content -raw /home/splunk/key.txt
 foreach($tickerID in $tick_list){
 $stock_api_url=("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+$tickerID+"&interval=1min&apikey="+$API_key+"&datatype=csv")
 $download_folder="/home/splunk/alphavantagedl/"
-$export_folder="/home/splunk/aplhavantagemon/"
+$export_folder="/home/splunk/alphavantagemon/"
 $date=Get-Date -Format dd-MM-yyyy-HH_mm
 $csv_transform_file=$download_folder+"csv_results"+$date+".csv"
 Invoke-RestMethod -Method Get $stock_api_url | Out-File -FilePath $csv_transform_file
